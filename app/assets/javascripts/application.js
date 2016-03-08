40f9bd8d22dcc8a3b8738a0823a148ad3c0f9b6d
Gregory Vandelune <vandelug@onid.oregonstate.edu>
2016-03-07 18:27:50 -0800

    added pages controller and tests, alsogmaps4rails to home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.turbolinks
+//= require bootstrap
+//= require underscore
+//= require gmaps/google
 //= require_tree .

6dfc1b37a266e0555a4016b55e42c17c3be510cb
Gregory Vandelune <vandelug@onid.oregonstate.edu>
2016-03-07 19:10:26 -0800

    added user model with secure password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root                          'pages#home'
   get    'about'          =>    'pages#about'
   get    'contact'        =>    'pages#contact'
+  get    'signup'         =>    'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

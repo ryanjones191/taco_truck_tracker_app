e869d5d84a1c9f8e1f18716e25de6b4ac33e270d
Gregory Vandelune <vandelug@onid.oregonstate.edu>
2016-03-10 19:46:47 -0800

    modified vendor profile page and fixed some routing issues

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,7 +17,7 @@ class ReviewsController < ApplicationController
 		@review.vendor_id = @vendor.id
 		respond_to do |format|
 			if @review.save
-				format.html { redirect_to root_path, notice: "Review was successfully created"}
+				format.html { redirect_to vendor_path(@vendor), notice: "Review was successfully created"}
 				format.json {render :show, status: :created, location: @review }
 			else
 				format.html { render :new }

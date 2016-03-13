4725e6f052a4a4165c96267daf780261dd5740e8
Gregory Vandelune <vandelug@onid.oregonstate.edu>
2016-03-12 23:05:56 -0800

    tweak ENV env with figaro for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["aws_access_key_id"],                        # required
-    aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
+    :provider                =>     'AWS',                        # required
+    :aws_access_key_id       =>     ENV["aws_access_key_id"],                        # required
+    :aws_secret_access_key   =>     ENV["aws_secret_access_key"],                        # required
     #region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
